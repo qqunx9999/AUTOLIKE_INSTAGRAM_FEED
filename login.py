1b342400cce9211b8f02b36a76b2d1affad4f646
mNLENIUm <mnlenium@kali.44.pool1.nky2-mser01.myaisfibre.com>
2020-06-13 20:18:51 +0700

    ver. 0.03

--- login.py
+++ login.py
@@ -20,8 +20,8 @@ class Login:
         self.write("#react-root > section > main > article > div.rgFsT > div:nth-child(1) > div > form > div:nth-child(2) > div > label > input",self.username)
         self.write("#react-root > section > main > article > div.rgFsT > div:nth-child(1) > div > form > div:nth-child(3) > div > label > input",self.password)
         self.write("#react-root > section > main > article > div.rgFsT > div:nth-child(1) > div > form > div:nth-child(4) > button > div")
-        self.write("#react-root > section > main > div > div > div > div > button")
-        self.write("body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.HoLwm")
+        self.write("#react-root > section > main > div > div > div > div > button",is_wait = "n")
+        self.write("body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.HoLwm",is_wait = "n")
         self.like_feed()
 
 
@@ -30,35 +30,42 @@ class Login:
         t = 0
         a = 0
         b = 0
-        while(b<=5):
-            t+=1
-            sleep(3)
-            #hearts = self.driver.find_elements_by_xpath("//button[@class='dCJp8 afkep coreSpriteHeartOpen _0mzm-' and @aria-label='Like']")
-            #hearts = self.driver.find_elements_by_xpath("//button[@class='dCJp8 afkep coreSpriteHeartOpen _0mzm-']")
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            self.driver.maximize_window()
-            #hearts = self.driver.find_elements_by_xpath('//span[@class="fr66n"]/svg[]')
-            #hearts = self.driver.find_elements_by_xpath('//avg[@aria-label="Like"]')
-            hearts = self.driver.find_elements_by_xpath('//span[@class="fr66n"]//*[@aria-label="Like"][@class="_8-yf5 "]')
-            sleep(5) if(len(hearts)==0) else sleep(1)
-            print(f"Run {t} time generate for {len(hearts)} likes")
-            for h in range(len(hearts)):
-                try:
-                    ActionChains(self.driver).move_to_element(hearts[h]).click(hearts[h]).perform()
-                except Exception:
-                    pass
-            if(len(hearts)==0):
-                a+=1
-            if(a>=5):
-                a = 0
-                b+=1
-                self.driver.get('https://www.instagram.com/')
-                print("reset")
-        print(f"Ending")
-
-    def write(self,content,text=""):
+        like = 0
         try:
-            uid = WebDriverWait(self.driver,10).until(EC.presence_of_element_located((By.CSS_SELECTOR,content)))
+            while(b<=5):
+                t+=1
+                sleep(3)
+                #hearts = self.driver.find_elements_by_xpath("//button[@class='dCJp8 afkep coreSpriteHeartOpen _0mzm-' and @aria-label='Like']")
+                #hearts = self.driver.find_elements_by_xpath("//button[@class='dCJp8 afkep coreSpriteHeartOpen _0mzm-']")
+                0 if(not(a)) else self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                self.driver.maximize_window()
+                #hearts = self.driver.find_elements_by_xpath('//span[@class="fr66n"]/svg[]')
+                #hearts = self.driver.find_elements_by_xpath('//avg[@aria-label="Like"]')
+                hearts = self.driver.find_elements_by_xpath('//span[@class="fr66n"]//*[@aria-label="Like"][@class="_8-yf5 "]')
+                like+=len(hearts)
+                sleep(3) if(len(hearts)==0) else sleep(1)
+                print(f"\rRun {t} times generate for {like} likes",end="")
+                for h in range(len(hearts)):
+                    try:
+                        ActionChains(self.driver).move_to_element(hearts[h]).click(hearts[h]).perform()
+                    except Exception:
+                        pass
+                if(len(hearts)==0):
+                    a+=1
+                if(a>=5):
+                    a = 0
+                    b+=1
+                    self.driver.get('https://www.instagram.com/')
+            print(f"Summary:\nTotal run {t} times you was {like} Likes")
+        except KeyboardInterrupt:
+            print(f"Stop program")
+            sleep(1)
+            print(f"Summary:\nTotal run {t} times you was {like} Likes")
+
+    def write(self,content,text="",is_wait = "y"):
+        try:
+            is_wait = 1 if(is_wait =="y") else 0
+            uid = WebDriverWait(self.driver,10).until(EC.presence_of_element_located((By.CSS_SELECTOR,content))) if(is_wait) else WebDriverWait(self.driver,4).until(EC.presence_of_element_located((By.CSS_SELECTOR,content)))
         except Exception:
             return 0
         uid.click()
